ce9c44bcbfc62ca44043abdcc5d2d1ba8dcc6ad1
jd580 <39346277+jd580@users.noreply.github.com>
2019-07-20 21:40:26 -0700

    Create Grouper.ps1

---
+++ Grouper.ps1
@@ -0,0 +1,29 @@
+#############################################################
+# Script to group files into directories based on extension.#
+#############################################################
+
+$path = Read-Host "Enter the path you would like to have sorted: "
+
+# Validate path.
+if (-not $(test-path -Path "$path")){
+    Write-Error -Message "Invalid Path."
+    Break
+}
+
+# Get the extensions in the path.
+Set-Location $path
+$files = Get-ChildItem -Path $path -File 
+$extensions = $files.Extension | Select-Object -Unique
+
+# Make directories for each type of extension.
+foreach ($extension in $extensions) {
+    if (-not $(test-path -Path "$path\$extension")){
+        New-Item -Path "$path\$extension" -ItemType Directory
+    }
+}
+
+# Move the files.
+foreach ($file in $files) {
+    Move-Item -Path $file.PSPath -Destination "$path\$($file.Extension)\"
+    Write-Host "Moving $file to $path\$($file.Extension)\$file"
+}
